bbd4b91e80f0b2069ebdb6c33730045b79fa167c
John Doe <benicio.vanzwijndregt@gmail.com>
2021-11-18 23:20:20 +1300

    Add project files.

---
+++ myRecipies6/myRecipies6/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using myRecipies6;
+using Todo.Views;
+
+namespace myRecipies6
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            //just have to make this page redirect to the todopage then test that then redo the code to fit assignment
+            InitializeComponent();
+            App.Current.MainPage = new NavigationPage(new Todo.Views.TodoListPage());
+
+        }
+        
+    }
+}
---
+++ myRecipies6/myRecipies6/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace myRecipies6.Models
+{
+    public class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string RecipeName { get; set; }
+        public string RecipeMaker { get; set; }
+        public string Ingredients { get; set; }
+        public string Steps { get; set; }
+    }
+}
